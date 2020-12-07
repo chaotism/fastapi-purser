1b07a8ba40bfa0c562f8721220779e63a9a7e260
Alexandr Bogut <a.bogut@semrush.com>
2020-12-07 05:32:19 +0300

    start writing basic secure functions

--- app/core/security/auths.py
+++ app/core/security/auths.py
@@ -1,1 +1,9 @@
 """API Authentications"""  # TODO: not implement
+
+
+def get_password_hash(password: str) -> int:
+    return hash(password)  # TODO: naive
+
+
+def verify_password(password: str, hashed_password: int) -> bool:
+    return get_password_hash(password) == hashed_password
