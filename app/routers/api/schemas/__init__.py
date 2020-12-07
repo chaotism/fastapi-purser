a6d6e281ab93c441acf1484305026c08de1f96dc
Alexandr Bogut <a.bogut@semrush.com>
2020-12-07 05:45:47 +0300

    write schemas for api

---
+++ app/routers/api/schemas/__init__.py
@@ -0,0 +1,4 @@
+from .transaction import Item, ItemCreate, ItemInDB, ItemUpdate
+from .msg import Msg
+from .token import Token, TokenPayload
+from .user import User, UserCreate, UserInDB, UserUpdate
