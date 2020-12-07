8d0c45b0bf45305fb21f8b134bd7ed2505dc5f42
Alexandr Bogut <a.bogut@semrush.com>
2020-12-07 05:34:42 +0300

    remove unused modules

--- app/core/routers/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.core.routers.hello import router as hello_router
-from app.utils.api.router import TypedAPIRouter
-
-hello_router = TypedAPIRouter(router=hello_router, prefix="/hello", tags=["hello"])
