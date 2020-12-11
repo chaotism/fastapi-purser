b03db1c42e983979cea1a71a0b450ca50c51485b
Alexandr Bogut <a.bogut@semrush.com>
2020-12-11 06:01:40 +0300

    fix method name

--- ddd/domain/users/servicies.py
+++ ddd/domain/users/servicies.py
@@ -12,7 +12,7 @@ class UserService(Service):
     def __init__(self, user_repo: UserRepository) -> None:
         self.user_repo = user_repo
 
-    def register_account(self, user_id: UserID, email: EmailStr, name: Optional[UserName] = None) -> User:
+    def register_user(self, user_id: UserID, email: EmailStr, name: Optional[UserName] = None) -> User:
         user = User(_id=user_id, email=email, name=name)  # TODO: create id after save
         self.user_repo.insert(user)
         return user
