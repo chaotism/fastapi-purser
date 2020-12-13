1c627f049a397142ba95fd36de2667dce453d94a
Alexandr Bogut <a.bogut@semrush.com>
2020-12-14 01:30:17 +0300

    minor update of service

--- application/domain/users/servicies.py
+++ application/domain/users/servicies.py
@@ -12,7 +12,15 @@ class UserService(Service):
     def __init__(self, user_repo: UserRepository) -> None:
         self.user_repo = user_repo
 
-    def register_user(self, user_id: UserID, email: EmailStr, name: Optional[UserName] = None) -> User:
-        user = User(_id=user_id, email=email, name=name)  # TODO: create id after save
-        self.user_repo.insert(user)
-        return user
+    def register_user(self, email: EmailStr, name: Optional[UserName] = None) -> User:
+        user_data = dict(
+            email=email,
+        )
+        if name is not None:
+            user_data['name'] = name
+        user = User(**user_data)  # TODO: create id after save
+        repo_user = self.user_repo.insert(user)
+        """
+        addition logic like send email etc
+        """
+        return repo_user
