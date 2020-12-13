c327ac50786e915705cbba5d15689250157bfe34
Alexandr Bogut <a.bogut@semrush.com>
2020-12-14 02:46:26 +0300

    update tests of service

--- tests/test_domain/test_users.py
+++ tests/test_domain/test_users.py
@@ -1,38 +1,83 @@
-from typing import Optional
+from typing import Optional, Dict
 from random import randint
-from uuid import uuid1
+from bson import ObjectId
+from contextlib import contextmanager
+
 import pytest
+from pydantic import ValidationError
+
+from domain.errors import EntityError
 from domain.users import UserID, User, UserService, UserRepository
 
 
-class FakeUserRepository(UserRepository):
-    data = {}
-
-    def get_by_id(self, instance_id: UserID) -> Optional[User]:
-        return self.data.get(instance_id)
-
-    def insert(self, instance: User) -> None:
-        self.data[User._id] = User
+class TestUserEnity:
+    """
+    tests with User fields validation
+    """
 
 
 class TestUserService:
     @pytest.fixture()
-    def user_repo(self):
-        return FakeUserRepository()
-
-    @pytest.fixture()
     def simple_user(self):
         return User(
-            _id=uuid1(),
+            _id=ObjectId(),
             email='user_{}@localhost.com'.format(randint(1, 100)),
             name='user_{}'.format(randint(1, 100)),
         )
 
-    def test_simple_positive_scenario(self, user_repo, simple_user):
-        user_service = UserService(user_repo)
-        user = user_service.register_user(user_id=simple_user._id, email=simple_user.email, name=simple_user.name,)
-        assert user.dict() == simple_user.dict()
-        assert user == user_service.user_repo.get_by_id(simple_user._id)
+    @pytest.fixture()
+    def fake_user_repo(self):
+
+        class FakeUserRepository(UserRepository):
+            data = {}
+
+            def get_by_id(self, instance_id: UserID) -> Optional[User]:
+                return self.data.get(instance_id)
+
+            def insert(self, instance: User) -> None:
+                instance.set_id(ObjectId())
+                self.data[instance.get_id()] = instance
+                return instance.get_id()
+
+            def update(self, instance: User) -> None:
+                if instance.get_id():
+                    self.data[instance.get_id()] = instance
+                raise EntityError('Null id')
+
+            def delete(self, instance: User) -> None:
+                if instance.get_id():
+                    self.data.pop(instance.get_id())
+                raise EntityError('Null id')
+
+            @contextmanager
+            def atomic(self):
+                try:
+                    yield
+                finally:
+                    pass
+
+        return FakeUserRepository()
+
+    def test_register_user_positive(self, fake_user_repo, simple_user):
+        user_service = UserService(fake_user_repo)
+        new_user = user_service.register_user(email=simple_user.email, name=simple_user.name,)
+        assert new_user.email == simple_user.email
+        assert new_user.name == simple_user.name
+        assert new_user == user_service.user_repo.get_by_id(new_user.id)
+
+    @pytest.mark.parametrize(
+        argnames="email,name",
+        argvalues=[
+            (None, 'Mario'),
+            (1, 'Luigi'),
+            ('cag;ca@cz', 'Bozer'),
+        ],
+        ids=['empty', 'int', 'wrong_email']
+    )
+    def test_register_user_negative(self, email, name, fake_user_repo):
+        user_service = UserService(fake_user_repo)
+        with pytest.raises(ValidationError) as err:
+            new_user = user_service.register_user(email=email, name=name,)
 
 
 class TestUserRepository:  # TODO: write test of base method of repos
