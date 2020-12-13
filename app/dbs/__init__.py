7f6cfc64dd9729126ba704e54dc4c3e0f4ca5bd4
Alexandr Bogut <a.bogut@semrush.com>
2020-12-13 18:21:16 +0300

    add db adapter

---
+++ app/dbs/__init__.py
@@ -0,0 +1,2 @@
+from .mongo import client as mongo_motor_client
+from .mongo import database as mongo_motor_database
--- app/dbs/mongo.py
+++ app/dbs/mongo.py
@@ -1,5 +1,5 @@
-import motor.motor_asyncio
+from motor import motor_asyncio
 from ..config import mongodb_config
 
-client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_config.uri)
+client = motor_asyncio.AsyncIOMotorClient(mongodb_config.uri)
 database = client[mongodb_config.db]
