cabef050a82d658c524727b7bdc9d99c033c4584
Alexandr Bogut <a.bogut@semrush.com>
2020-12-13 05:22:39 +0300

    fix app config

--- app/config/application.py
+++ app/config/application.py
@@ -10,5 +10,5 @@ class ApplicationSettings(BaseSettings):  # TODO: not used fields
     is_test: bool = Field(True, env="API_TEST")
     is_debug: bool = Field(True, env="API_DEBUG")
 
-    port: int = Field(8000, env="API_TEST")
-    host: str = Field("0.0.0.0", env="API_TEST")
+    host: str = Field("0.0.0.0", env="API_HOST")
+    port: int = Field(8000, env="API_PORT")
