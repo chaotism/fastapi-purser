42636390bb0ddae542ed1a169579bfee02ae9431
Alexandr Bogut <a.bogut@semrush.com>
2020-12-14 04:17:48 +0300

    add AccountRepository

--- application/domain/accounts/repositories.py
+++ application/domain/accounts/repositories.py
@@ -5,7 +5,8 @@ from typing import Optional
 from .entities import Account
 from .types import AccountID
 from ..errors import EntityError
-from domain.types import Repository
+from ...domain.types import Repository
+from ...dbs.mongo import client, database as motor_database
 
 
 class AccountRepository(Repository):
@@ -31,3 +32,36 @@ class AccountRepository(Repository):
     @contextmanager
     def atomic(self):
         pass
+
+
+class MotorAccountRepository(AccountRepository):
+    collection = motor_database.transactions
+
+    async def get_by_id(self, instance_id: AccountID) -> Optional[Account]:
+        transaction = await self.collection.find_one({'_id': instance_id})
+        return Account(**transaction)
+
+    async def insert(self, instance: Account) -> AccountID:
+        data = instance.dict(by_alias=True)
+        result = await self.collection.insert_one(data)
+        return result.inserted_id
+
+    async def update(self, instance: AccountID) -> None:
+        instance_id = instance.get_id()
+        if instance_id:
+            data = instance.dict(by_alias=True)
+            data.pop('_id')
+            result = await self.collection.update_one({'_id': instance_id}, {'$set': data})
+        raise EntityError('Null id')
+
+    async def delete(self, instance: Account) -> None:
+        instance_id = instance.get_id()
+        if instance_id:
+            await self.collection.delete_one({'_id': instance_id})
+        raise EntityError('Null id')
+
+    @asynccontextmanager
+    async def atomic(self):
+        async with await client.start_session() as s:
+            async with s.start_transaction():
+                yield
