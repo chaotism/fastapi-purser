dda9eaf296f5dbab0fc0026713b9139c7beb152b
Alexandr Bogut <a.bogut@semrush.com>
2020-12-15 02:25:16 +0300

    minor fix

--- application/server/core/middleware/middlewares.py
+++ application/server/core/middleware/middlewares.py
@@ -1,9 +1,7 @@
 """API Middlewares"""  # TODO: not implement
 import time
-
-from fastapi import FastAPI, Request
-
-app = FastAPI()
+from fastapi import Request
+from application.server.app import app
 
 
 @app.middleware('http')
