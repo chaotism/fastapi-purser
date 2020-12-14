d87a31cd1fe9c0d9078adb0537a3221dfaf9f527
Alexandr Bogut <a.bogut@semrush.com>
2020-12-14 04:56:13 +0300

    write method for getting list of transaction

--- application/domain/accounts/servicies.py
+++ application/domain/accounts/servicies.py
@@ -1,7 +1,9 @@
+from typing import List
 from .entities import Account, Money
 from .repositories import AccountRepository
 from .types import AccountID
-from ..users.entities import User
+from ..users import User
+from ..transactions import Transaction, TransactionService
 from ...domain.types import Service
 
 
@@ -21,3 +23,8 @@ class AccountService(Service):
     def withdraw(self, account: Account, money: Money):
         account.balance.amount -= money.amount
         self.account_repo.update(account)
+
+    @staticmethod
+    def get_account_transactions(transaction_service: TransactionService, account_id: AccountID) -> List[Transaction]:
+        transactions = transaction_service.transaction_repo.get_by_account_id(account_id)
+        return transactions
