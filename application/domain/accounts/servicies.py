aeba46c0bddc30333eb869d5c1cc9c964de978eb
Alexandr Bogut <a.bogut@semrush.com>
2020-12-15 02:30:09 +0300

    cosmetic fixes

--- application/domain/accounts/servicies.py
+++ application/domain/accounts/servicies.py
@@ -1,19 +1,26 @@
 from typing import List
-from .entities import Account, Money
+
+from ..errors import EntityError
+from ..transactions import Transaction
+from ..transactions import TransactionService
+from ..types import Service
+from ..users import User
+from .entities import Account
+from .entities import Money
 from .repositories import AccountRepository
 from .types import AccountID
-from ..users import User
-from ..transactions import Transaction, TransactionService
-from ..types import Service
-from ..errors import EntityError
 
 
 class AccountService(Service):
     def __init__(self, account_repo: AccountRepository) -> None:
         self.account_repo = account_repo
 
-    def register_account(self, account_id: AccountID, user: User, balance: Money) -> Account:
-        account = Account(_id=account_id, owner=user, balance=balance)  # TODO: create id after save
+    def register_account(
+        self, account_id: AccountID, user: User, balance: Money
+    ) -> Account:
+        account = Account(
+            _id=account_id, owner=user, balance=balance
+        )  # TODO: create id after save
         self.account_repo.insert(account)
         return account
 
@@ -26,19 +33,18 @@ class AccountService(Service):
         self.account_repo.update(account)
 
     @staticmethod
-    def get_account_transactions(transaction_service: TransactionService, account_id: AccountID) -> List[Transaction]:
-        transactions = transaction_service.transaction_repo.get_by_account_id(account_id)
+    def get_account_transactions(
+        transaction_service: TransactionService, account_id: AccountID
+    ) -> List[Transaction]:
+        transactions = transaction_service.transaction_repo.get_by_account_id(
+            account_id
+        )
         return transactions
 
     @staticmethod
-    def get_account_transactions(transaction_service: TransactionService, account_id: AccountID) -> List[Transaction]:
-        transactions = transaction_service.transaction_repo.get_by_account_id(account_id)
-        return transactions
-
-    @staticmethod
-    def is_account_owner(account: Account, checking_user: User) -> bool:
+    def is_account_owner(account: Account, current_user: User) -> bool:
         if account.owner.id is None:
             raise EntityError("account owner haven't id'")
-        if checking_user.id is None:
+        if current_user.id is None:
             raise EntityError("checking_user haven't id'")
-        return account.owner.id == checking_user.id
+        return account.owner.id == current_user.id
--- application/domain/users/servicies.py
+++ application/domain/users/servicies.py
@@ -2,11 +2,11 @@ from typing import Optional
 
 from pydantic import EmailStr
 
+from ..errors import EntityError
+from ..types import Service
+from ..users import User
 from .repositories import UserRepository
 from .types import UserName
-from ..users import User
-from ..types import Service
-from ..errors import EntityError
 
 
 class UserService(Service):
@@ -14,11 +14,9 @@ class UserService(Service):
         self.user_repo = user_repo
 
     def register_user(self, email: EmailStr, name: Optional[UserName] = None) -> User:
-        user_data = dict(
-            email=email,
-        )
+        user_data = dict(email=email)
         if name is not None:
-            user_data['name'] = name
+            user_data["name"] = name
 
         user = User(**user_data)
         repo_user_id = self.user_repo.insert(user)
@@ -26,16 +24,16 @@ class UserService(Service):
         return self.user_repo.get_by_id(repo_user_id)
 
     @staticmethod
-    def is_yourself(you: User, checking_user: User) -> bool:
+    def is_yourself(you: User, current_user: User) -> bool:
         if you.id is None:
             raise EntityError("you haven't id'")
-        if checking_user.id is None:
+        if current_user.id is None:
             raise EntityError("checking_user haven't id'")
-        return you.id == checking_user.id
+        return you.id == current_user.id
 
     @staticmethod
-    def is_superuser(checking_user: User) -> bool:
-        return checking_user.is_super_user
+    def is_superuser(current_user: User) -> bool:
+        return current_user.is_super_user
 
     def have_users(self) -> bool:
         return self.user_repo.get_count() > 0
