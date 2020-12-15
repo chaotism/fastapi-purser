ed19f9d827d867ca6f96d97846c799b8aed9bd96
Alexandr Bogut <a.bogut@semrush.com>
2020-12-15 04:08:36 +0300

    remove unused modules

--- application/server/routers/api/__init__.py
+++ application/server/routers/api/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from server.routers.api.endpoints import transaction, users
+from server.routers.api.endpoints import transactions, users
 
 api_router = APIRouter(prefix='/api')
 
 
 api_router.include_router(users.router, prefix='/users', tags=['users'])
-api_router.include_router(transaction.router, prefix='/items', tags=['items'])
+api_router.include_router(transactions.router, prefix='/transactions', tags=['items'])
