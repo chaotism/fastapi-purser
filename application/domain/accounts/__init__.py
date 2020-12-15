c7ab231666af53ed3c1e21431c4e754608e9222c
Alexandr Bogut <a.bogut@semrush.com>
2020-12-15 04:08:01 +0300

    add motor repo to root import

--- application/domain/accounts/__init__.py
+++ application/domain/accounts/__init__.py
@@ -1,4 +1,4 @@
 from .entities import Account, Money
 from .servicies import AccountService
-from .repositories import AccountRepository
+from .repositories import AccountRepository, MotorAccountRepository
 from .types import AccountID
--- application/domain/users/__init__.py
+++ application/domain/users/__init__.py
@@ -1,4 +1,4 @@
 from .entities import User
 from .servicies import UserService
-from .repositories import UserRepository
+from .repositories import UserRepository, MotorUserRepository
 from .types import UserID
