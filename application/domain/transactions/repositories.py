edebff0477da10f42f5e813562138341b0724b71
Alexandr Bogut <a.bogut@semrush.com>
2020-12-16 02:16:41 +0300

    rewrite transaction to async

--- application/domain/transactions/repositories.py
+++ application/domain/transactions/repositories.py
@@ -15,40 +15,49 @@ class TransactionRepository(Repository):
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    def get_by_id(self, instance_id: TransactionID) -> Optional[Transaction]:
+    async def get_count(self) -> int:
         pass
 
     @abstractmethod
-    def get_by_account_id(self, account_id: AccountID) -> Optional[List[Transaction]]:
+    async def get_by_id(self, instance_id: TransactionID) -> Optional[Transaction]:
         pass
 
     @abstractmethod
-    def insert(self, instance: Transaction) -> None:
+    async def get_many_by_account_id(self, account_id: AccountID) -> Optional[List[Transaction]]:
         pass
 
     @abstractmethod
-    def update(self, instance: Transaction) -> None:
+    async def insert(self, instance: Transaction) -> None:
         pass
 
     @abstractmethod
-    def delete(self, instance: Transaction) -> None:
+    async def update(self, instance: Transaction) -> None:
+        pass
+
+    @abstractmethod
+    async def delete(self, instance: Transaction) -> None:
         pass
 
     @abstractmethod
     @contextmanager
-    def atomic(self):
+    async def atomic(self):
         pass
 
 
 class MotorTransactionRepository(TransactionRepository):
     collection = motor_database.transactions
 
+    async def get_count(self) -> int:
+        return self.collection.count_documents({})
+
     async def get_by_id(self, instance_id: TransactionID) -> Optional[Transaction]:
         transaction = await self.collection.find_one({'_id': instance_id})
+        if transaction is None:
+            return
         return Transaction(**transaction)
 
-    async def get_by_account_id(self, account_id: AccountID) -> Optional[List[Transaction]]:
-        transactions_data = await self.collection.find(
+    async def get_many_by_account_id(self, account_id: AccountID, buff_size: int = 1000) -> Optional[List[Transaction]]:
+        transactions_data_cursor = self.collection.find(
             {
                 '$or': [
                     {'from_account.id': account_id},
@@ -56,26 +65,30 @@ class MotorTransactionRepository(TransactionRepository):
                 ]
             }
         )
+        transactions_data = [data for data in await transactions_data_cursor.to_list(length=buff_size)]
+        if transactions_data is None:
+            return
         return list(map(lambda transaction: Transaction(**transaction), transactions_data))
 
     async def insert(self, instance: Transaction) -> TransactionID:
         data = instance.dict(by_alias=True)
+        data.pop('_id')
         result = await self.collection.insert_one(data)
         return result.inserted_id
 
     async def update(self, instance: Transaction) -> None:
         instance_id = instance.get_id()
-        if instance_id:
-            data = instance.dict(by_alias=True)
-            data.pop('_id')
-            await self.collection.update_one({'_id': instance_id}, {'$set': data})
-        raise EntityError('Null id')
+        if not instance_id:
+            raise EntityError('Null id')
+        data = instance.dict(by_alias=True)
+        data.pop('_id')
+        await self.collection.update_one({'_id': instance_id}, {'$set': data})
 
     async def delete(self, instance: Transaction) -> None:
         instance_id = instance.get_id()
-        if instance_id:
-            await self.collection.delete_one({'_id': instance_id})
-        raise EntityError('Null id')
+        if not instance_id:
+            raise EntityError('Null id')
+        await self.collection.delete_one({'_id': instance_id})
 
     @asynccontextmanager
     async def atomic(self):
--- application/domain/transactions/servicies.py
+++ application/domain/transactions/servicies.py
@@ -7,35 +7,36 @@ from ..errors import EntityError
 from ..accounts import AccountID, Account, AccountService
 from .entities import Transaction, Money
 from .repositories import TransactionRepository
-from .types import TransactionID
 
 
 class TransactionService(Service):
     def __init__(self, transaction_repo: TransactionRepository) -> None:
         self.transaction_repo = transaction_repo
 
-    def register_transaction(self, from_account: Account, to_account: Account, amount: Money) -> Transaction:
-        transaction = Transaction(from_account=from_account, to_account=to_account, sum=amount)
-        repo_transaction_id = self.transaction_repo.insert(transaction)
-        return self.transaction_repo.get_by_id(repo_transaction_id)
+    async def register_transaction(self, from_account: Account, to_account: Account, amount: Money) -> Transaction:
+        async with self.transaction_repo.atomic():
+            transaction = Transaction(from_account=from_account, to_account=to_account, sum=amount)
+            repo_transaction_id = await self.transaction_repo.insert(transaction)
+            return await self.transaction_repo.get_by_id(repo_transaction_id)
 
-    def evaluate_transaction(self, account_service: AccountService, transaction: Transaction) -> Transaction:
-        if not transaction.id:
+    async def evaluate_transaction(self, account_service: AccountService, transaction: Transaction) -> Transaction:
+        if not transaction.get_id():
             raise EntityError('Null id')
-        if not self.transaction_repo.get_by_id(Transaction.id):
+        if not self.transaction_repo.get_by_id(transaction.get_id()):
             raise EntityError('Not exists')
         try:
-            with account_service.account_repo.atomic():
-                account_service.withdraw(transaction.from_account, transaction.sum)
-                account_service.deposit(transaction.to_account, transaction.sum)
+            async with account_service.account_repo.atomic():
+                await account_service.withdraw(transaction.from_account, transaction.sum)
+                await account_service.deposit(transaction.to_account, transaction.sum)
                 transaction.set_complete()
-                self.transaction_repo.update(transaction)
+                await self.transaction_repo.update(transaction)
         except Exception as err:
             logger.error(str(err))  # TODO: use logger
             transaction.set_failed(str(err))
-            self.transaction_repo.update(transaction)
+            await self.transaction_repo.update(transaction)
+            raise
         return transaction
 
-    def get_account_transaction(self, account_id: AccountID) -> List[Transaction]:
-        transactions = self.transaction_repo.get_by_account_id(account_id)
+    async def get_account_transaction(self, account_id: AccountID) -> List[Transaction]:
+        transactions = await self.transaction_repo.get_many_by_account_id(account_id)
         return transactions
