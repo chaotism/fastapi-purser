924b809e32f2a6d404e2b6d29b380b4c8b1cdf69
Alexandr Bogut <a.bogut@semrush.com>
2020-12-16 03:39:18 +0300

    update tests

--- application/tests/test_api/test_app_status.py
+++ application/tests/test_api/test_app_status.py
@@ -3,18 +3,18 @@ from tests.client import TestClient
 
 
 class TestSystemStatus:
-    prefix = '/system-status/'
+    prefix = '/health/app_info'
     client = TestClient(prefix, app)
 
-    def test_get(self):
-        response = self.client.get('/post')
+    def test_get_status(self):
+        response = self.client.post('/system-status/')
         assert 'ok' in response.text.lower()
 
 
 class TestAppVersion:
-    prefix = '/app-version/'
+    prefix = '/health/app_info'
     client = TestClient(prefix, app)
 
-    def test_get(self):
-        response = self.client.get('/post')
+    def test_get_version(self):
+        response = self.client.post('/app-version/')
         assert '0.1' in response.text.lower()
